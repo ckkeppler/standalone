358896f6ef0107121666ca9dc85a607a9d0667d8
Chris Keppler <ckkeppler@gmail.com>
2023-03-20 11:56:45 -0400

    finish standalone app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Route, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+import { WelcomeComponent } from './welcome/welcome.component';
+
+const routes: Route[] = [
+  {
+    path: '',
+    component: WelcomeComponent,
+  },
+  {
+    path: 'about',
+    // component: AboutComponent,
+    // lazy load standalone component, import returns a promise
+    loadComponent: () =>
+      import('./about/about.component').then((mod) => mod.AboutComponent),
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./dashboard/routes').then((mod) => mod.DASHBOARD_ROUTES),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
